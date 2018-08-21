07142ddf41c989b1fb9c3fbf65c353dbd17315ac
Alex Gutierrez <0alexgutierrez@gmail.com>
2018-08-21 09:35:35 -0700

    first coulpe high order function drills

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -35,14 +35,14 @@ function average(numbers) {
     sum += number;
   });
 
-  return sum / numbers.length
+  return sum / numbers.length;
 }
 
 console.log(average([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 console.log(average([0, -1, 1]));
 
 function repeat(fn, n) {
-  for (let i=0; i<n; i++) {
+  for (let i = 0; i < n; i++) {
     fn();
   }
 }
@@ -57,3 +57,55 @@ function goodbye() {
 
 repeat(hello, 5);
 repeat(goodbye, 5);
+
+function filter(arr, fn) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i])) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that
+  // only returns a boolean
+  return name[0] === 'R';
+});
+
+console.log(filteredNames); // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+function hazardWarningCreator(typeOfWarning) {
+  let warningCounter = 0;
+  return function(location) {
+    warningCounter++;
+    console.log(`Danger! There is a ${typeOfWarning} hazard at ${location}!`);
+    if (warningCounter === 1) {
+      console.log(
+        `The ${typeOfWarning} hazard alert has triggered ${warningCounter} time today!`
+      );
+    }
+    if (warningCounter >= 2) {
+      console.log(
+        `The ${typeOfWarning} hazard alert has triggered ${warningCounter} times today!`
+      );
+    }
+  };
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const iceWarning = hazardWarningCreator('Ice on the Road');
+const deerWarning = hazardWarningCreator('Deer on the Road');
+
+rocksWarning('Main St and Pacific Ave');
+iceWarning('Mill Ave');
+deerWarning('Pacific Drive');
+rocksWarning('Atlantic Ave');
